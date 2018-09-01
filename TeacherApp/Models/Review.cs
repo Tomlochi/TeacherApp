3721e9d9ac0b5b40d4e64d57360c3b12f19cbb68
Liran Ziv <liran27@gmail.com>
2018-09-02 02:24:59 +0300

    update db schema

--- TeacherApp/Models/Review.cs
+++ TeacherApp/Models/Review.cs
@@ -11,9 +11,9 @@ namespace TeacherApp.Models
         public DateTime Published { get; set; }
         public int Rating { get; set; }
         public Teacher Teacher { get; set; }
-        public int TeacherID { get; set; }
+        public int? TeacherID { get; set; }
         public string ReviewContent { get; set; }
         public Person Person { get; set; }
-        public int PersonID { get; set; }
+        public int? PersonID { get; set; }
     }
 }
