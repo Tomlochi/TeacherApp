315dc779168a38aae15215bb6b2d6d6703a12c07
Liran Ziv <liran27@gmail.com>
2018-09-01 15:47:48 +0300

    - fix db seeding - continue query dev - add 'AddReview' method to teacher class

--- TeacherApp/wwwroot/js/searchBestOffer.js
+++ TeacherApp/wwwroot/js/searchBestOffer.js
@@ -1,38 +1,35 @@
 ﻿$(document).ready(function () {
-    // set btn name
-    // set input name
-    // set tbBody on html page
+    console.log("document ready. loading js searchBestOffer..");
     // consider drop down list in view couses and maybe search by id
-    $("#search").on('click',
-        function (event) {
-            console.log("clicked");
-            console.log($("#searchParam").val());
-            $.ajax(
-                {
-                    type: "POST",
-                    url: "UserPanel/SearchBestOffer",
-                    contentType: "application/json; charset=utf-8",
-                    data: { courseName: $("#searchParam").val() },
-                    dataType: 'json',
-                    success: function (response) {
-                        console.log("success")
-                        $.each(response, function (i, elm) {
-                            for (i = 0; i < resp.data.length; i++) {
-                                trHTML +=
-                                    '<tr><td>'
-                                    + resp.elm[i].teaher
-                                    + '</td><td>'
-                                    + resp.elm[i].price
-                                    + '</td><td>'
-                            }
-                        });
-                        $('#results').append(trHTML);
-                    },
-                    error: function (req, status, err) {
-                        console.log('something went wrong', status, err);
-                    }
+    $("#search").click(function () {
+        console.log("Searching best offer for " + $("#searchParam").val() + " course");
+        $.ajax(
+            {
+                type: "POST",
+                url: "UserPanel/SearchBestOffer",
+                //contentType: "application/json; charset=utf-8",
+                data: { courseName: $("#searchParam").val() },
+                //dataType: 'json',
+                success: function (response) {
+                    console.log("Prepering results for presentation")
+                    //window.location.replace("http://localhost:55264/UserPanel/SearchBestOffer");
+                    var trHTML = '';
+                    $.each(response, function (i, elm) {
+                        for (i = 0; i < resp.data.length; i++) {
+                            trHTML +=
+                                '<tr><td>'
+                                + resp.elm[i].teaher
+                                + '</td><td>'
+                                + resp.elm[i].price
+                                + '</td><td>'
+                        }
+                    });
+                    $('#results').append(trHTML);
                 },
-            )
-        }
-    )
+                error: function (req, status, err) {
+                    console.log('something went wrong', status, err);
+                }
+            },
+        )
+    });
 });
