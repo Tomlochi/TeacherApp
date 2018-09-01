269c0c5fbfcdcb248a168bcbf2de76de25404f3b
Tom Lochi <tom.lochi@gmail.com>
2018-09-01 15:46:31 +0300

    user login

--- TeacherApp/wwwroot/js/Admin.js
+++ TeacherApp/wwwroot/js/Admin.js
@@ -38,21 +38,6 @@ function setCookie(cname, cvalue, exdays) {
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
-function getUserNameFromCookie() {
-    var name = "Email";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        ;
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
 
 function removeCookies() {
     var res = document.cookie;
